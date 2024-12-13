542578483b1b80700d5c2c4a6a853e3ad21663c1
Qudratjon1 <qudratovqudratjon5@gmail.com>
2024-12-13 19:48:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const name = prompt("Iltimos ismingizni kiriting:");
+const spaceId = prompt("Iltimos Space ID ni kiriting:");
+const spacePassword = prompt("Iltimos Space Password ni kiriting:");
+const userInfo = {
+  name: name,
+  id: spaceId,
+  password: spacePassword
+};
+console.log(userInfo);
